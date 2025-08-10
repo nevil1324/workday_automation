0a93bb71bc2d8d060f08b3ad22fdbfc63a2d923b
Nevil Sakhreliya <80466571+nevil1324@users.noreply.github.com>
2025-08-10 14:10:09 +0530

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-# config.py
-NEXT_BUTTON_SELECTORS = [
-    'button[data-automation-id="pageFooterNextButton"]',
-    'button[data-automation-id="saveAndContinueButton"]',
-    'button[data-automation-id="nextButton"]',
-]
